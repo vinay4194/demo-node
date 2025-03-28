3af5e56ec40e1771395c07372cf72a830d9a504d
vinay4194 <v.sangwan96@gmail.com>
2025-03-28 20:32:28 +0530

    port

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ mongoose
 		console.log("Error on db connection", err);
 	});
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, "0.0.0.0", () => {
 	console.log(`Server running on port ${PORT}`);
 });
